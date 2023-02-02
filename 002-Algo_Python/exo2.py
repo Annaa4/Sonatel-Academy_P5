08590fb3a1a4d55448c56b60680d02ce75af5bba
Anna <annasow296@gmail.com>
2023-02-02 08:09:52 +0000

    Tasks

--- 002-Algo_Python/exo2.py
+++ 002-Algo_Python/exo2.py
@@ -1,3 +1,9 @@
 p = str (input("Veuiller entrer votre phrase"))
 p1=""
-  
+for i in range (0, len(p)):
+    if p [i] == " " and p[i+1] == " ":
+     pass 
+    else :
+        p1 = p1+p[i]
+print (p)
+print(p1) 
