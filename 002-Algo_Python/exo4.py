6e75800740c08464d6003a6ef61e97cd0f12a380
Anna <annasow296@gmail.com>
2023-02-06 15:41:51 +0000

    modif

--- 002-Algo_Python/exo4.py
+++ 002-Algo_Python/exo4.py
@@ -1,5 +1,2 @@
-numero = int(input("Entrer vos numeros"))
-def num_valide(a):
-    for p in numero:
-        if p[0] in [7] and p[1] in [0,7,8]:
-            return p 
+numero =int (input("Donner les numéros, veuillez les séparer par des virgules"))
+
