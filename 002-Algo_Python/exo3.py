70e022df7bdb743b2cef004ca5a17316b59074cb
Anna <annasow296@gmail.com>
2023-02-06 15:06:33 +0000

    Modif

--- 002-Algo_Python/exo3.py
+++ 002-Algo_Python/exo3.py
@@ -1,22 +1,37 @@
-phrase = str(input("Entrer vos phrases:\n")) 
-while phrase == "":
-    input ("Entrer vos phrases:\n")
-    
-def recuperation_des_phrases():
-    for p in phrase :
-        p = p + phrase
-        if((p >='A' or p <='Z') and (p in [".","!","?"])):
-           pass
-        else:
-            
-         print (p)
+phrase = " Neymar da Silva    S'   antos        Júnior, dit Ney'mar Jr. ,est un footballeur international brésilien qui évolue au poste d' attaquant au Paris Saint-Germain ."
+# while phrase == "":
+#     input ("Entrer vos phrases:\n") 
 
-def caracteres_speciaux() :
-    p1=""
-    for i in range (0, len(phrase)):
-        if phrase[i] == " " and phrase[i+1] == " ":
-            pass 
-        else :
-            p1 = p1+phrase[i]
-    print (phrase)
-    print(p1) 
+def espaces_inutiles(x):
+    o=""
+    for i in range(len(x)):
+        if not (x[i] ==" " and x[i+1]== " "):
+            o=o+x[i]
+    return o 
+
+ph_1= espaces_inutiles(phrase)
+print(ph_1)
+
+
+def caracteres_speciaux(x):
+    a = ''
+    for i in range (len(x)):
+        if ((x[i] >= 'A' and x[i] < 'Z') or(x[i] >= 'a' and x[i] < 'z') or(x[i] in [",","'",":"," ",";","|","()","/","-","é"])):
+            a =a+x[i]
+    return a 
+n = caracteres_speciaux(ph_1)
+print(n)
+    
+def recuperation_des_phrases(x):
+    t=[]
+    a=0
+    for i in range (len(x)) : 
+        if(x[i] in [".","!","?"]):
+           g = x[a:i+1]
+           if len(g)>=25:
+               t.append(g) 
+               a = i+1    
+    return t
+
+v=recuperation_des_phrases(n)
+print(v)
--- 002-Algo_Python/exo4.py
+++ 002-Algo_Python/exo4.py
@@ -1,2 +1,5 @@
-numero = int (input("Entrer les numéros que vous souhaitez"))
-    
+numero = int(input("Entrer vos numeros"))
+def num_valide(a):
+    for p in numero:
+        if p[0] in [7] and p[1] in [0,7,8]:
+            return p 
