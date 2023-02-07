79b2504b52226e307b619c085b557668aee9e545
Anna <annasow296@gmail.com>
2023-02-07 20:21:15 +0000

    Modif

--- 002-Algo_Python/exo7.py
+++ 002-Algo_Python/exo7.py
@@ -1,5 +1,4 @@
-# phrase = (input("Entrer votre phrase"))
-phrase = 'bonjour aly'
+phrase = (input("Entrer votre phrase"))
 m = {'a':'2','b':'22','c':'222','d':'3','e':'33','f':'333','g':'4',
      'h':'44','i':'444','j':'5','k':'55','l':'555','m':'6','n':'66',
      'o':'666','p':'7','q':'77','r':'777','s':'7777','t':'8','u':'88',
@@ -22,5 +21,3 @@ for i in range(1,len(r)):
 
 for i in t:
     print(i,end="")
-
-
