4be6a1f594db929195b14718736f281f84a9ab44
Anna <annasow296@gmail.com>
2023-02-10 10:30:19 +0000

    Modif

--- 002-Algo_Python/exo8.py
+++ 002-Algo_Python/exo8.py
@@ -7,12 +7,13 @@ elif choisir == 2 :
     print('\n')
     print ('La liste des étudiants :\n' )
     affichage(tab)
-elif choisir == 3 : print('Voici le tri par ordre décroissant de la moyenne :\n')
+elif choisir == 3 : 
+    print('Voici le tri par ordre décroissant de la moyenne :\n')
+    tri(tab)
 elif choisir == 4 : 
     choix = input('Donner un critère de recherche d"un étudiant: ')
     print('\n')
     recherche(choix,tab)
-elif choisir == 4 : print('Donner un critère de recherche d"un étudiant : ')
 elif choisir == 5 : 
     choix = input('Vous souhaitez modifier les informations de quel étudiant ? Veuiller donner son numero : ')
     modification(choix,tab)
