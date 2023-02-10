562b225052c53cc0bd97e47a9ed21082168ee69e
Anna <annasow296@gmail.com>
2023-02-10 08:53:15 +0000

    Modif

--- 002-Algo_Python/exo8.py
+++ 002-Algo_Python/exo8.py
@@ -1,9 +1,33 @@
 from mesfonctionsexo8 import *
-choisir = int (input("        MENU\n 1. Ajouter un autre étudiant\n 2. Trier et afficher \n 3. Rechercher\n 4. Modification de notes\n 5. Quitter\n Veuillez faire votre choix :\n"))
-if choisir == 1 : print
-elif choisir == 2 :print('Trier')
-elif choisir == 3 : print('Rechercher')
-elif choisir == 4 : print('Modification')
-elif choisir == 5 : exit
-# while choisir != 5 :
-    # choisir = int (input("          MENU\n 1. Ajouter un autre étudiant\n 2. Trier et afficher \n 3. Rechercher\n 4. Modification de notes\n 5. Quitter\n Veuillez faire votre choix :\n"))
+tab = [['Prenom','Nom','Téléphone','Classe','Dev','Proj','Exam','Moyenne']]
+choisir = int (input("        MENU\n 1. Ajouter un étudiant\n 2.Afficher tout\n 3.Trier et afficher \n 4. Rechercher\n 5. Modification de notes\n 6. Quitter\n Veuillez faire votre choix : "))
+if choisir == 1 : 
+    saisie(tab)
+elif choisir == 2 :
+    print('\n')
+    print ('La liste des étudiants :\n' , affichage(tab))
+elif choisir == 3 : print('Voici le tri par ordre décroissant de la moyenne :\n')
+elif choisir == 4 : 
+    choix = input('Donner un critère de recherche d"un étudiant: ')
+    print('\n')
+    recherche(choix,tab)
+elif choisir == 4 : print('Donner un critère de recherche d"un étudiant : ')
+elif choisir == 5 : print('Vous souhaitez modifier les informations de quel étudiant ? Veuiller donner son numero : ')
+elif choisir == 6 : exit
+while choisir != 6:
+    print('\n')
+    choisir = int (input("        MENU\n 1. Ajouter un étudiant\n 2.Afficher tout\n 3.Trier et afficher \n 4. Rechercher\n 5. Modification de notes\n 6. Quitter\n Veuillez faire votre choix :\n"))
+    if choisir == 1 : 
+        print('\n')
+        saisie(tab)
+    elif choisir == 2 :
+        print('\n')
+        print ('La liste des étudiants :\n' , affichage(tab))
+    elif choisir == 3 : print('Voici le tri par ordre décroissant de la moyenne :\n')
+    elif choisir == 4 : 
+        choix = input('Donner un critère de recherche d"un étudiant : ')
+        print('\n')
+        recherche(choix,tab)
+    elif choisir == 4 : print('Donner un critère de recherche d"un étudiant: ')
+    elif choisir == 5 : print('Vous souhaitez modifier les informations de quel étudiant ? : Veuiller donner son numero : ')
+if choisir == 6 : exit
